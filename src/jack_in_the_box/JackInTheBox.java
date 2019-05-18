8c62dc29b656534f85e0178455ebf6e13c657158
League Student <league@iMac.attlocal.net>
2019-05-18 11:30:39 -0700

    update
    small change

--- src/jack_in_the_box/JackInTheBox.java
+++ src/jack_in_the_box/JackInTheBox.java
@@ -1,5 +1,61 @@
 package jack_in_the_box;
 
-public class JackInTheBox {
+import java.applet.AudioClip;
+import java.net.URL;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+public class JackInTheBox {
+	static int t;
+	public static void main(String[] args) {
+		t = 0; //unfinished
+		JFrame frame = new JFrame();
+		JButton button = new JButton();
+		frame.add(button);
+		button.setText("SUPRISE");
+		frame.pack();
+		button.addActionListener(this);
+		
+	}
+	private void showPicture(String fileName) { 
+	     try {
+	          JLabel imageLabel = createLabelImage(fileName);
+	          JFrame frame = new JFrame();
+	          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	          frame.add(imageLabel);
+	          frame.setVisible(true);
+	          frame.pack();
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+
+	private JLabel createLabelImage(String fileName) {
+	     try {
+	          URL imageURL = getClass().getResource(fileName);
+	          if (imageURL == null) {
+	               System.err.println("Could not find image " + fileName);
+	               return new JLabel();
+	          } else {
+	               Icon icon = new ImageIcon(imageURL);
+	               JLabel imageLabel = new JLabel(icon);
+	               return imageLabel;
+	          }
+	     } catch (Exception e) {
+	          System.err.println("Could not find image " + fileName);
+	          return new JLabel();
+	     }
+	}
+	private void playSound(String soundFile) { 
+	     try {
+	          AudioClip sound = JApplet.newAudioClip(getClass().getResource(soundFile));
+	          sound.play();
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
 }
